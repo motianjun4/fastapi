c32f1961984bd451c072d72de07c6dc95c87bee0
motianjun4 <motianjun4@gmail.com>
2022-01-13 20:38:22 -0500

    add uvicorn

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 from fastapi import FastAPI 
-
+import uvicorn
+    
 app = FastAPI()
 
 @app.get("/")
 async def root():
     return {"message": "Hello World!"}
 
-    
+uvicorn.run(app, host="0.0.0.0", port=8000)
