d9b214329036d3d33c8bf492b9b4dd5eeb455bf1
motianjun4 <motianjun4@gmail.com>
2022-01-19 14:42:40 -0500

    add: add func

--- main.py
+++ main.py
@@ -7,4 +7,10 @@ app = FastAPI()
 async def root():
     return {"message": "Hello World!"}
 
+@app.get("/calc/add/")
+async def read_item(a: int = None, b: int = None):
+    if a is None or b is None:
+        return {"message": "Please specify query parameters a:int and b:int"}
+    return a+b
+
 uvicorn.run(app, host="0.0.0.0", port=8000)
